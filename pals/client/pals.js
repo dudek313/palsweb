6b5da97bec29ad2a5b24e25d3ab68debee1ce9bd
Eden Duthie <eduthie@gmail.com>
2013-06-07 18:00:49 +1000

    Added router

--- pals/client/pals.js
+++ pals/client/pals.js
@@ -1,16 +1,8 @@
+Meteor.Router.add({
+  '/home': 'home',
+  '/workspaces': 'workspaces',
+  '*': 'home'
+});
+
 Meteor.startup(function(){
-
-    var currentView = 'home';
-
-    var views = {};
-    views['home'] = Meteor.render(function(){ return Template.home() });
-    views['workspaces'] = Meteor.render(function(){ return Template.workspaces() });
-    
-    window['changeView'] = function(viewName) {
-        currentView = viewName;
-        console.log(views[currentView]);
-        $('#main').html(views[currentView]);
-    };
-    
-    window['changeView']('home');
 });
