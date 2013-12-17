ab4c8e42c24d1d4a9188b9b286a515dcf775e389
Eden Duthie <eduthie@gmail.com>
2013-12-17 16:15:54 +1100

    About to remove met and flux components

--- pals/server/data.js
+++ pals/server/data.js
@@ -14,7 +14,7 @@ if( !reference ) {
         'vegType' : ['Evergreen broadleaf','urban'],
         'filePickerAPIKey' : 'AeyxZFgbpSHS6xZ5AfPJkz',
         'spatialLevel' : ['SingleSite', 'MultipleSite', 'Catchment', 'Regional', 'Global'],
-        'timeStepSize' : ['hourly','daily','monthly','yearly'],
+        'timeStepSize' : ['hourly','daily','monthly','annual'],
         'stateSelection' :['values measured at site','model spin-up on site data','default model initialisation','other'],
         'parameterSelection' : ['automated calibration','manual calibration','no calibration (model default values)','other']
     });
