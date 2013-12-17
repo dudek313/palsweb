389e09f8789164a5818686d5a3641df557605a0b
Eden Duthie <eduthie@gmail.com>
2013-12-17 17:55:02 +1100

    Playing with analysis display

--- pals/client/controller/analysis.js
+++ pals/client/controller/analysis.js
@@ -3,3 +3,28 @@ Template.analysis.analysis = function() {
     var currentAnalysis = Analyses.findOne({'_id':currentAnalysisId});
     return currentAnalysis;
 }
+
+/*
+Template.analysis.helpers({
+  stripes: function(array, fn, elseFn) {
+  if (array && array.length > 0) {
+    var buffer = "";
+    for (var i = 0, j = array.length; i < j; i++) {
+      var item = array[i];
+ 
+      item.first = ( i == 0 ? true : false );
+      item.third = (i % 3 == 0 ? true : false);
+      item.last = ( i == j - 1 ? true : false);
+
+      buffer += fn(item);
+    }
+ 
+    // return the finished buffer
+    return buffer;
+  }
+  else {
+    return elseFn();
+  }
+}
+});
+*/
