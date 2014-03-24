8bc22ccb56c0b05b10e5ea7ced0d2809cb81d003
Eden Duthie <eduthie@gmail.com>
2014-03-24 04:19:17 +0000

    Added fields to exepriemnts and datasets

--- pals/server/data.js
+++ pals/server/data.js
@@ -14,9 +14,10 @@ if( !reference ) {
         'vegType' : ['Evergreen broadleaf','urban'],
         'filePickerAPIKey' : 'AeyxZFgbpSHS6xZ5AfPJkz',
         'spatialLevel' : ['SingleSite', 'MultipleSite', 'Catchment', 'Regional', 'Global'],
-        'timeStepSize' : ['hourly','daily','monthly','annual'],
+        'timeStepSize' : ['single value','half-hourly','hourly','daily','monthly','annual'],
         'stateSelection' :['values measured at site','model spin-up on site data','default model initialisation','other'],
-        'parameterSelection' : ['automated calibration','manual calibration','no calibration (model default values)','other']
+        'parameterSelection' : ['automated calibration','manual calibration','no calibration (model default values)','other'],
+        'resolution' : ['0.01 deg','0.5 deg','1 deg','2 deg','Other']
     });
 }
 
