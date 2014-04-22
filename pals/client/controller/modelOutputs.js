8212933db72a90c2f043221671dd9f8ee6167043
Eden Duthie <eduthie@gmail.com>
2014-04-22 02:59:12 +0000

    Updated version and fixed model outputs table

--- pals/client/controller/modelOutputs.js
+++ pals/client/controller/modelOutputs.js
@@ -1,12 +1,20 @@
 Template.modelOutputs.modelOutputs = function() {
     var user = Meteor.user();
     if( user ) {
+        var modelOutputs;
         var modelOutputs = ModelOutputs.find({'workspaces':user.profile.currentWorkspace._id}).fetch();
         if( modelOutputs )
         {
             modelOutputs.forEach(function(modelOutput){
-                if( modelOutput.owner ) modelOutput.owner = Meteor.users.findOne({'_id':modelOutput.owner},{'_id':1,'name':1});
-                if( modelOutput.experiment ) modelOutput.experiment = Experiments.findOne({'_id':modelOutput.experiment},{'_id':1,'name':1});
+                if( modelOutput.owner ) {
+                    modelOutput.owner = 
+                        Meteor.users.findOne({'_id':modelOutput.owner},{'_id':1,'name':1});
+                    if( modelOutput.owner ) {
+                        modelOutput.owner.email = modelOutput.owner.emails[0].address;        
+                    }
+                }
+                if( modelOutput.experiment ) modelOutput.experiment = 
+                    Experiments.findOne({'_id':modelOutput.experiment},{'_id':1,'name':1});
             });
         }
         return modelOutputs;
