5fbc4102ce0ca6e34f7bb0d8562394cdbb894d86
Eden Duthie <eduthie@gmail.com>
2014-09-15 12:00:43 +0000

    Fixed models view

--- scripts/datamigration.js
+++ scripts/datamigration.js
@@ -46,7 +46,7 @@ var mongo = function() {
     that.mongo = require('mongodb');
     that.host = 'localhost';
     that.port = 81;
-    that.db = new that.mongo.Db('meteor',new that.mongo.Server(that.host,that.port,[]),{fsync:true});
+    that.db = new that.mongo.Db('meteor',new that.mongo.Server(that.host,that.port,[]),{fsync:true,journal : true});
     
     function connect(callback) {
         console.log('connecting to mongo');
@@ -104,7 +104,8 @@ var mongo = function() {
 };
 
 function copyFile(source, target, cb) {
-    fs.copy(source,target,cb);
+    fs.copySync(source,target,cb);
+    cb();
 }
 
 function loadUsers(mongoI,callback) {
@@ -211,6 +212,116 @@ function processDataSets(users,mongoInstance,workspaces,pgInstance,publicWorkspa
     });
 }
 
+function processModelOutputs(users,mongoInstance,workspaces,pgInstance,publicWorkspace) {
+
+    console.log("Processing model outputs");
+
+    var loadModelOutputsQuery =
+
+    "SELECT \
+    mo.accesslevel as mo_accesslevel, \
+    mo.modelid as mo_modelid, \
+    mo.parameterselection as mo_parameterselection, \
+    mo.stateselection as mo_stateselection, \
+    mo.uploaddate as mo_uploaddate, \
+    mo.username as mo_username, \
+    mo.id as mo_id, \
+    mo.usercomments as mo_usercomments, \
+    dsv.id as dsv_id, \
+    e.experiment_id AS e_id, \
+    a.name AS a_name,\
+    a.status AS a_status,\
+    a.owner_username AS a_owner, \
+    ds.id as ds_id \
+    FROM modeloutput as mo \
+    INNER JOIN datasetversion as dsv \
+    ON dsv.id = mo.datasetversionid \
+    INNER JOIN dataset as ds \
+    ON ds.id = dsv.datasetid \
+    INNER JOIN analysable as a \
+    ON a.id = mo.id \
+    INNER JOIN experimentable as e \
+    ON e.id = mo.id;";
+
+    pgInstance.sql(loadModelOutputsQuery,function(result,client){
+        result.rows.forEach(function(row){
+            var filename = baseDir + '/' + row.mo_username + '/' + 'mo' + row.mo_id + '.nc';
+            fs.exists(filename, function (exists) {
+                if( exists ) {
+                    console.log('File exsits: ' + filename);
+                    // copy the file
+                    var newFilename = uuid.v4();
+                    fs.stat(filename,function(err,stats){
+                        if( err ) {
+                            console.log('could not stat ' + filename)
+                        }
+                        else {
+                            var fileData = {
+                                path : palsDataDir + '/' + newFilename,
+                                filename : 'mo' + row.mo_id + '.nc',
+                                size : stats['size'],
+                                key : newFilename,
+                                created : row.mo_uploaddate
+                            }
+                            console.log(fileData);
+                            user = users[row.mo_username];
+                            if( user ) {
+                                copyModelOutput(filename,fileData,row,user,mongoInstance,workspaces,publicWorkspace);
+                            }
+                            else console.log('Could not locate username ' + row.ds_username);
+                        }
+                    });
+                }
+            });
+        })
+        client.end();
+    });
+}
+
+numberOfCopies = 0;
+maxCopies = 100;
+
+function copyModelOutput(filename,fileData,row,user,mongoInstance,workspaces,publicWorkspace) {
+    console.log('Copying model output: ' + row.a_name);
+    copyFile(filename,fileData.path,function(err){
+        if( err ) console.log(err);
+        else {
+            var modelOutput = {
+                _id : uuid.v4(),
+                comments : row.mo_usercomments,
+                created : row.mo_uploaddate,
+                model : row.mo_modelid.toString(),
+                name : row.a_name,
+                owner : user._id,
+                parameterSelection : row.mo_parameterselection,
+                stateSelection : row.mo_stateselection,
+                accessLevel : row.mo_accesslevel,
+                status : row.a_status,
+                versions : [fileData],
+                experiment : row.ds_id.toString()
+            }
+            if( row.e_id ) {
+                modelOutput.workspaces = [row.e_id.toString()];
+            }
+            else {
+                modelOutput.workspaces = [publicWorkspace._id.toString()];
+            }
+            console.log(modelOutput);
+            mongoInstance.insert('modelOutputs',modelOutput,function(err){
+                if(err) {
+                    console.log(err);
+                    console.log('Trying with different name');
+                    modelOutput.name = modelOutput.name + '.1';
+                    mongoInstance.insert('modelOutputs',modelOutput,function(err){
+                        if( err ) console.log(err);
+                    });
+                }
+            });
+
+        }
+    });
+}
+
 function copyDataSet(filename,fileData,row,user,mongoInstance,workspaces,publicWorkspace) {
     console.log('Copying data set: ' + row.a_name);
     copyFile(filename,fileData.path,function(err){
@@ -306,6 +417,19 @@ function loadWorkspaces(pgInstance,callback) {
     });
 }
 
+function loadModels(pgInstance,callback) {
+    var loadModelsQuery = "SELECT * FROM model";
+    
+    pgInstance.sql(loadModelsQuery,function(result,client){
+        var models = [];
+        result.rows.forEach(function(row){
+            models.push(row);
+        });
+        callback(models);
+    });
+    
+}
+
 function loadSharedList(pgInstance,workspace,callback) {
     var loadSharedListQuery = "SELECT sharedlist_username FROM experiment_palsuser WHERE experiments_id = " + workspace.id;
     
@@ -399,6 +523,64 @@ function loadPublicWorkspace(mongoInstance,callback) {
     });
 }
 
+function loadAndCopyModels(pgInstance,mongoInstance,users,publicWorkspace,callback) {
+    console.log('Loading and copying models');
+    loadModels(pgInstance,function(models){
+        var waiting = models.length;
+        var mongoModels = [];
+        for( var i=0; i < models.length; ++i ) {
+            model = models[i];
+            var user = users[model.ownerusername];
+            if( !user ) {
+                console.log('Could not find user: ' + model.owner_username);
+            }
+            else {
+                var mongoModel = {
+                    _id : model.id.toString(),
+                    name : model.modelname,
+                    owner : user._id,
+                    workspaces : [publicWorkspace._id]
+                }
+                mongoModels.push(mongoModel);
+            }
+        }
+        for( var i=0; i < mongoModels.length; ++i ) {
+            saveModel(mongoInstance,mongoModels[i],function(){
+                --waiting;
+                if( waiting <=0 ) {
+                    callback(mongoModels);
+                }
+            });
+        }
+    });
+    
+}
+
+function saveModel(mongoInstance,model,callback) {
+    mongoInstance.findOne('models',{_id:model._id},function(err,doc){
+        if( err ) console.log(err);
+        if( doc ) {
+            console.log('Already have model with id ' + model._id);
+            callback();
+        }
+        else {
+            mongoInstance.findOne('models',{name:model.name},function(err,doc2){
+                if( err ) console.log(err);
+                if( doc2 ) {
+                    console.log('Already have model with name ' + model.name + ' trying new name');
+                    model.name = model.name + '(new)';
+                }
+                console.log('Model name: ' + model.name);
+                mongoInstance.insert('models',model,function(err){
+                    if( err ) console.log(err);
+                    callback();
+                });
+            });
+        }
+    });
+
+}
+
 // var mongoInstance = mongo();
 // mongoInstance.find('users',{},function(docs,db){
 //     docs.each(function(err,doc){
@@ -421,7 +603,11 @@ function process() {
                             loadPublicWorkspace(mongoInstance,function(err,result){
                                 if( err ) console.log(err);
                                 else {
-                                    processDataSets(users,mongoInstance,workspaces,pgInstance,result);
+                                    //processDataSets(users,mongoInstance,workspaces,pgInstance,result);
+                                    loadAndCopyModels(pgInstance,mongoInstance,users,result,function(models){
+                                        console.log('Models copied')
+                                        processModelOutputs(users,mongoInstance,workspaces,pgInstance,result);
+                                    });
                                 }
                             });
                         });
