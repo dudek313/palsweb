bbf5294786c963ce89cf2e7c7c165a2e04ddda05
Eden Duthie <eduthie@gmail.com>
2014-09-27 02:20:23 +0000

    Added scripts for country and vegtype

---
+++ scripts/country.js
@@ -0,0 +1,47 @@
+var postgres = function () {
+
+    that = {};
+    
+    that.pg = require('pg');
+    that.connectionString = "pg://postgres:password@localhost:5432/pals"
+
+    function sql(query,callback) {
+        that.client.query(query,null,function(err,result){
+            if( err ) console.log(err);
+            else callback(result,that.client);
+        });
+    }
+    that.sql = sql;
+    
+    function connect(callback) {
+        console.log('connecting to postgres');
+        that.pg.connect(that.connectionString,function(err,client){
+            that.client = client;
+            console.log('connected to postgres');
+            if(err) console.log(err);
+            callback(err);
+        });
+    }
+    that.connect = connect;
+    
+    function end() {
+        if( that.pg ) {
+            that.pg.end();
+        }
+    }
+    that.end = end;
+
+    return that;
+}
+
+var pgInstance = postgres();
+pgInstance.connect(function(err){
+    query = "select name from country";
+    pgInstance.sql(query,function(result,client){
+        output = '';
+        result.rows.forEach(function(row){
+            output += "'" + row.name + "',";
+        });
+        console.log(output);
+    });   
+});
