15e4a679e255ec201103f44e535bdfc04e601ddb
Danny Eisenberg <ravdanny@gmail.com>
2016-09-10 13:50:28 +0000

    added tests

---
+++ pals/client/controller/dataset.app-test.js
@@ -0,0 +1,138 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
+
+import { chai } from 'meteor/practicalmeteor:chai';
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+import 'meteor/meteor';
+import { sinon } from 'meteor/practicalmeteor:sinon';
+
+
+import { withRenderedTemplate } from './helpers.app-test.js';
+import '../../both/collections.js';
+import './datasets.js';
+
+describe('Data set tests', function(done) {
+
+  before(function(done) {
+    Meteor.loginWithPassword('gabsun@gmail.com', 'password', function(err) {
+      try {
+        chai.assert.isUndefined(err);
+        var user = Meteor.user();
+        chai.assert.equal(user.emails[0].address, 'gabsun@gmail.com');
+      } catch(error) {
+        done(error);
+      }
+    });
+
+    Meteor.call('test.resetDatabase', done);
+
+  });
+
+  after(function(done) {
+    Meteor.logout(function(err) {
+      try {
+        chai.assert.isUndefined(err);
+        var user = Meteor.user();
+        chai.assert.isNull(user);
+      } catch(error) {
+        done(error);
+      }
+      done();
+    });
+  });
+
+  describe('Create a new data set', function() {
+    before(function() {
+      var screenMode = { params: { screenMode: 'create' }};
+      sinon.stub(Router, 'current', () => screenMode);
+    });
+
+    after(function() {
+      Router.current.restore();
+    })
+
+    it('allows creation of a data set', function(done) {
+      withRenderedTemplate('dataSet', {}, el => {
+        $(el).find('input[name="name"]').val("Data Set X");
+        chai.assert.equal($(el).find('input[name="name"]').val(), "Data Set X");
+        $(el).find('select[name="type"]').val("flux tower");
+        chai.assert.equal($(el).find('select[name="type"]').val(), "flux tower");
+        $(el).find('select[name="spatialLevel"]').val("SingleSite");
+        chai.assert.equal($(el).find('select[name="spatialLevel"]').val(), "SingleSite");
+        $(el).find('button.upload-btn').click();
+        setTimeout(null, 300);
+//        $(el).find('input[type="file"]').val("C:\\Users\\Danny\\Downloads\\pals-nci\\webappdata\\axel\\mo24829.nc");
+        var createBtn = $(el).find('button.create-btn');
+        chai.assert.notEqual(createBtn, [], 'Save button is not appearing');
+        $(el).find('button.create-btn').click();
+        setTimeout(function() {
+          var newDataSet = DataSets.findOne({name: 'Data Set X'});
+          chai.assert.isDefined(newDataSet);
+          done();
+        }, 1000);
+
+      });
+    });
+
+  });
+
+/*  describe('Data set display list page', function(done) {
+    before(function(done) {
+      var newDataSet = makeDataSet("Data Set 2");
+      Meteor.call('insertDataSet', newDataSet, function(err, dsId) {
+        try {
+          chai.assert.isUndefined(err);
+          var insertedDataSet = DataSets.findOne({_id: dsId});
+          chai.assert.equal(insertedDataSet.length, 1);
+        } catch(error) {
+          done(error);
+        }
+      });
+      var resolution = { params: {resolution: 'All', source: 'anywhere' }};
+      sinon.stub(Router, 'current', () => resolution);
+      done();
+    });
+
+    after(function(done) {
+      Router.current.restore();
+      done();
+    })
+
+    it('displays a data set', function(done) {
+      Router.go('/dataSets/anywhere/All');
+
+      withRenderedTemplate('dataSets', {}, el => {
+        console.log($(el).find('tr'));
+        chai.assert.equal($(el).find('tr').length, 2, 'wrong number of rows in table');
+        chai.assert.equal($(el).find('td')[0].textContent, "Data Set 2");
+        done();
+      });
+    });
+
+  });
+
+*/
+
+});
+
+
+
+
+function makeModel(modelName) {
+  var model = {
+    name: modelName
+  };
+
+  return model;
+}
+
+function makeDataSet(dataSetName) {
+  var dataSet = {
+    name: dataSetName,
+    type: 'flux tower',
+    spatialLevel: 'SingleSite'
+  };
+
+  return dataSet;
+}
---
+++ pals/client/controller/upload.app-test.js
@@ -0,0 +1,107 @@
+/* eslint-env mocha */
+/* eslint-disable func-names, prefer-arrow-callback */
+
+import { chai } from 'meteor/practicalmeteor:chai';
+import { Template } from 'meteor/templating';
+import { $ } from 'meteor/jquery';
+import 'meteor/meteor';
+import { sinon } from 'meteor/practicalmeteor:sinon';
+
+
+import { withRenderedTemplate } from './helpers.app-test.js';
+import '../../both/collections.js';
+import './dataset.js';
+
+describe('Testing file upload', function(done) {
+  before(function(done) {
+    Meteor.call('test.resetDatabase', done);
+
+    // login admin user to create new user
+    Meteor.loginWithPassword('gabsun@gmail.com', 'password', function(err) {
+      try {
+        chai.assert.isUndefined(err);
+        var user = Meteor.user();
+        chai.assert.equal(user.emails[0].address, 'gabsun@gmail.com');
+      } catch(error) {
+        done(error);
+      }
+    });
+
+    //create test user
+    Meteor.call('test.createUser', {email:'test0@testing.com', password: 'password1', profile: {firstName: "test", lastName: "test"}}, function(err) {
+      try {
+        chai.assert.isUndefined(err);
+      } catch(error) {
+        done(error);
+      }
+    });
+
+    // logout admin user
+    Meteor.logout(function(err) {
+      try {
+        chai.assert.isUndefined(err, 'Logout created error');
+        var user = Meteor.user();
+        chai.assert.isNull(user, 'User is still logged in');
+      } catch(error) {
+        done(error);
+      }
+    });
+
+    // login test user
+    Meteor.loginWithPassword('gabsun@gmail.com', 'password', function(err) {
+      try {
+        chai.assert.isUndefined(err);
+        var user = Meteor.user();
+        chai.assert.equal(user.emails[0].address, 'gabsun@gmail.com');
+      } catch(error) {
+        done(error);
+      }
+    });
+
+    // simulate router creating a data set
+    var screenMode = { params: { screenMode: 'create' }};
+    sinon.stub(Router, 'current', () => screenMode);
+    done();
+
+  //  sinon.stub(this, 'currentUpload', () => new ReactiveVar(false);
+  });
+
+  after(function(done) {
+    // remove test user
+    var testUser = Meteor.users.findOne({'profile.fullname':'test test'});
+    if (testUser) {
+      Meteor.call('test.removeUser', {_id: testUser._id}, function(err, docId) {
+        console.log('Test user removed');
+        done(err);
+      });
+      done(); // temporary - until I work out the problem
+    }
+    else done();
+
+    // restore router settings
+    Router.current.restore();
+  });
+
+  describe('Upload a file', function() {
+    it('allows a registered user to upload a file', function(done) {
+
+    });
+  });
+/*  describe('Upload a file', function() {
+    it('allows a registered user to upload a file', function(done) {
+      withRenderedTemplate('dataSet', {}, el => {
+        $(el).find('button.upload-btn').click();
+        var downloadPanel = $(el).find('#upload-panel');
+        chai.assert.isAbove(downloadPanel.length, 0, 'Download panel not displaying');
+        $(el).find('input[type="file"]').val("C:\\Users\\Danny\\Downloads\\pals-nci\\webappdata\\axel\\mo24829.nc");
+
+        setTimeout(function() {
+          var newFile = Files.findOne({name: 'mo24829.nc'});
+          chai.assert.isDefined(newFile);
+          done();
+        }, 5000);
+      });
+    });
+  });
+  */
+});
